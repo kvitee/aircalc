0ca769bc1ed9ed46d8986cf4020b953ec2bcfcf4
kvitee <kvitee41@gmail.com>
2024-10-03 21:55:04 +0800

    Implement calculate method of Formula class.

--- src/formula.cpp
+++ src/formula.cpp
@@ -1,22 +1,85 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
 
 #include <QtCore/QString>
+#include <QtCore/QPair>
 #include <QtCore/QMap>
+#include <QtCore/QStack>
 
 #include "formula.hpp"
 
 
-Formula::Formula(const QString &expression)
-  : m_expression(expression) {}
+Formula::Formula(const QString &expr)
+  : m_expr(expr) {}
+
+const QMap<QString, double> Formula::m_consts = {
+  { "pi", M_PIl },
+  { "e", M_El },
+};
+
+const QMap<QChar, QPair<quint8, Formula::fn_t>> Formula::m_ops = {
+  { '+', { 1, [](double a, double b) { return a + b; } }},
+  { '-', { 1, [](double a, double b) { return a - b; } }},
+  { '*', { 2, [](double a, double b) { return a * b; } }},
+  { '/', { 2, [](double a, double b) { return a / b; } }},
+  { '^', { 3, [](double a, double b) { return std::pow(a, b); } }},
+};
 
 double Formula::calculate(const QMap<QString, double> &args) {
-  std::cout << "Calculating "
-            << m_expression.toStdString()
-            << " with arguments:\n";
+  auto vals = QStack<double>();
+  auto ops = QStack<QPair<quint32, fn_t>>();
 
-  for (auto key : args.keys()) {
-    std::cout << " - " << key.toStdString() << " = " << args[key] << '\n';
+  const auto reduce = [&vals, &ops](quint32 mpr = 0) {
+    while (!ops.empty() && ops.top().first >= mpr) {
+      double b = vals.pop();
+      double a = vals.pop();
+
+      vals.push(ops.pop().second(a, b));
+    }
+  };
+
+  auto ids = QMap<QString, double>();
+
+  ids.insert(args);
+  ids.insert(m_consts);
+
+  quint32 bp = 0;
+
+  const std::string s_expr = m_expr.toLower().toStdString();
+  for (const char *ch = s_expr.c_str();; ++ch) {
+    while (*ch == '(') {
+      bp += 3; ++ch;
+    }
+
+    char *end;
+    double num = strtod(ch, &end);
+
+    if (ch == end) {
+      while (QChar(*end).isLetterOrNumber() || *end == '_') ++end;
+
+      num = ids.value(QString::fromUtf8(ch, end - ch));
+    }
+
+    ch = end;
+    vals.push(num);
+
+    while (*ch == ')') {
+      bp -= 3; ++ch;
+    }
+
+    if (!*ch) break;
+
+    auto op = m_ops.value(*ch);
+
+    quint32 p = bp + op.first;
+    reduce(p);
+
+    ops.push({p, op.second});
   }
 
-  return 0.0;
+  reduce();
+
+  return vals.pop();
 }
--- src/include/formula.hpp
+++ src/include/formula.hpp
@@ -2,18 +2,26 @@
 #define FORMULA_HPP_INCLUDED_
 
 
+#include <functional>
+
 #include <QtCore/QString>
 #include <QtCore/QMap>
+#include <QtCore/QPair>
 
 
 class Formula {
   public:
-    Formula(const QString &expression);
+    Formula(const QString &expr);
 
     double calculate(const QMap<QString, double> &args);
 
   protected:
-    const QString m_expression;
+    const QString m_expr;
+
+    using fn_t = std::function<double (double, double)>;
+
+    static const QMap<QString, double> m_consts;
+    static const QMap<QChar, QPair<quint8, fn_t>> m_ops;
 };
 
 
